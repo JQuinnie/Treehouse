0221b09648918471a5c5f8c132a294e7d03a1dcf
JQuinnie <jenn8866@gmail.com>
2017-12-14 15:15:13 -0600

    included funciton to pull in values

--- Dynamic-Node-Website/renderer.js
+++ Dynamic-Node-Website/renderer.js
@@ -1,10 +1,20 @@
 var fs = require('fs');
 
+function mergeValues(values, content) {
+  // cycle over the keys
+  for (var key in values) {
+    // replace all {{key}} with the value from the values object
+    content = content.replace('{{' + key + '}}', values[key]); // same as --> values.avatarUrl === values['avatarUrl']
+  }
+    // return merged content
+    return content;
+}
+
 function view(templateName, values, response) {
   // read from template files
-  var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+  var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: 'utf8'});
   // insert valves into the content
-
+  fileContents = mergeValues(values, fileContents);
   // write out the contents to the responses
   response.write(fileContents);
 }
