c112ac23f8dd9776011ade3af0e0657a0c7733d0
JQuinnie <jenn8866@gmail.com>
2017-12-14 13:28:02 -0600

    establish connection with localhost

--- Dynamic-Node-Website/app.js
+++ Dynamic-Node-Website/app.js
@@ -2,6 +2,16 @@
 // Solution: Use Node.js to perform the profile look ups and server our template via HTTP
 
 // Create web server
+const http = require('http');
+const PORT = 3000;
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  setInterval(function(){
+    response.write(new Date() + '\n');
+  }, 1000);
+  response.end('Hello World\n');
+}).listen(PORT);
+console.log('Server running at http://<workspace-url>/'); // for localhost, go to browser at 'localhost:3000'
 
 // Handle HTTP route GET / and POST / i.e. Home
   // if url == "/" && GET
