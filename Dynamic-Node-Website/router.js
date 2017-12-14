d322b614b3527ae0ee85aa33b34aea38f0646fe2
JQuinnie <jenn8866@gmail.com>
2017-12-14 14:07:40 -0600

    adding values to the user function

--- Dynamic-Node-Website/router.js
+++ Dynamic-Node-Website/router.js
@@ -1,3 +1,5 @@
+var Profile = require('./profile.js');
+
 // Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   // if url == "/" && GET
@@ -19,13 +21,30 @@ function user(request, response) {
   if (username.length > 0) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Header\n');
-    response.write(username + '\n');
-    response.end('Footer\n');
+
     // get json from Treehouse
-      // on "end"
-        // show profile
-      // on "error"
-        // show error
+    var studentProfile = new Profile(username);
+    // on "end"
+    studentProfile.on('end', function(profileJSON) {
+      // show profile
+
+      // store the values which we need
+      var values = {
+        avatarUrl: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascript: profileJSON.points.JavaScript
+      }
+      // simple response
+      response.write(`${values.username} has ${values.badges} badges\n`);
+      response.end('Footer\n');
+    });
+    // on "error"
+    studentProfile.on('error', function (error) {
+      // show error
+      response.write(error.message + '\n');
+      response.end('Footer\n');
+    });
   }
 }
 
