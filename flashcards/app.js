48a28f4dd91a4022c25e1dd5eb8041458a8a0c68
JQuinnie <jenn8866@gmail.com>
2017-12-15 11:07:45 -0600

    adding intial js for flashcards app

---
+++ flashcards/app.js
@@ -0,0 +1,20 @@
+// require the Express module and assign to variable express
+const express = require('express');
+// call express, express function returns an Express application, assigned to variable app
+const app = express();
+// tells express which template engine to use, default it will look into the views folder
+app.set('view engine', 'pug');
+// get the route
+app.get('/', (req, res) => {
+  res.send('<H1>I love TreeHouse!</H1>');
+});
+// make second page route
+app.get('/hello', (req, res) => {
+  res.send('<H1>Hello JavaScript Developer!</H1>');
+});
+// set up development server using the listen method with port number 3000
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
+});
+// ABOVE code will create a server and when it is run, the server will run on machine
+
