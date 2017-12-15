6798450541a718593e8d11f2aec73b25a8d1ece9
JQuinnie <jenn8866@gmail.com>
2017-12-15 11:09:27 -0600

    updating app to include pug render as well as npm pug index file

--- flashcards/app.js
+++ flashcards/app.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 // tells express which template engine to use, default it will look into the views folder
 app.set('view engine', 'pug');
-// get the route
+// get the route, using render and pug
 app.get('/', (req, res) => {
-  res.send('<H1>I love TreeHouse!</H1>');
+  res.render('index');
 });
 // make second page route
 app.get('/hello', (req, res) => {
@@ -17,4 +17,3 @@ app.listen(3000, () => {
   console.log('The application is running on localhost:3000');
 });
 // ABOVE code will create a server and when it is run, the server will run on machine
-
