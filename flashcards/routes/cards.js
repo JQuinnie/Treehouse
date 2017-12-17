3267e9d72bcfd41248d82c1f4c3b5a60f3448e98
JQuinnie <jenn8866@gmail.com>
2017-12-17 09:34:09 -0600

    adding data in json to work with app

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// require the data
+const data = require('../data/flashcardData.json').data; // same way written as const {data} = ...
+const cards = data.cards; // same way written as const {cards} = data
+
 // make second page route
-router.get('/', (req, res) => {
-  res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whoes tomb it is"});
+router.get('/:id', (req, res) => { // ':' tells express to treat this part of the url as a variable, stored in request object param property
+  res.render('card', {
+    prompt: cards[req.params.id].question, // using id property on params
+    hint: cards[req.params.id].hint});
 });
 
 module.exports = router;
