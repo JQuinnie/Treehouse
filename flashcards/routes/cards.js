9fd0bd6d8604f07d3a1b224d9360721a0c9233c6
JQuinnie <jenn8866@gmail.com>
2017-12-17 14:18:23 -0600

    adding logic to redirect user to a random card, adding next card link

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -5,14 +5,26 @@ const router = express.Router();
 const data = require('../data/flashcardData.json').data; // same way written as const {data} = ...
 const cards = data.cards; // same way written as const {cards} = data
 
+// including logic to randomize cards to display
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardID = Math.floor(Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardID}`);
+})
+
 // make second page route
 router.get('/:id', (req, res) => { // ':' tells express to treat this part of the url as a variable, stored in request object param property
   const {side} = req.query;
   const {id} = req.params;
+
+  if(!side) {
+    res.redirect(`/cards/${id}?side=question`) // if side does not exist on the card number, it will redirect
+  }
+  const name = req.cookies.username;
   const text = cards[id][side];
   const {hint} = cards[id];
 
-  const templateData = {id, text};
+  const templateData = {id, text, name};
   // if statement for the hint to only show up when on the question side of the card, switch between the sides to show
   if (side === 'question') {
     templateData.hint = hint;
