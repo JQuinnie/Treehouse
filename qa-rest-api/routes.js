dd33680cf127f04d790bf31cce8133ac1a1bad8f
JQuinnie <jenn8866@gmail.com>
2018-01-04 14:54:06 -0600

    updated routes for GET POST PUT DELETE for each question and answer combination

--- qa-rest-api/routes.js
+++ qa-rest-api/routes.js
@@ -18,11 +18,54 @@ router.post('/', function(req, res) {
   });
 });
 
-// GET /questions/:id
+// GET /questions/:qID
 // reoute for creating questions
-router.get('/:id', function(req, res) {
+router.get('/:qID', function(req, res) {
   res.json({
     response: 'You sent me a GET request for ID ' + req.params.id
+  });
+});
+
+// POST /questions/:qID/answers
+// reoute for creating an answer
+router.post('/:qID/answers', function(req, res) {
+  res.json({
+    response: 'You sent me a POST request to /answers',
+    questionId: req.params.qID,
+    body: req.body
+  });
+});
+
+// PUT /questions/:qID/answers/:aID
+// edit a specific answer
+router.put('/:qID/answers/:aID', function(req, res) {
+  res.json({
+    response: 'You sent me a PUT request to /answers',
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    body: req.body
+  });
+});
+
+// DELETE /questions/:qID/answers/:aID
+// delete a specific answer
+router.delete('/:qID/answers/:aID', function(req, res) {
+  res.json({
+    response: 'You sent me a DELETE request to /answers',
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+  });
+});
+
+// POST /questions/:qID/answers/:aID/vote-up
+// POST /questions/:qID/answers/:aID/vote-down
+// Vote on a specific answer
+router.post('/:qID/answers/:aID/vote-:dir', function(req, res) { //dir = direction
+  res.json({
+    response: 'You sent me a POST request to /vote-' + req.params.dir,
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    vote: req.params.dir
   });
 });
 
