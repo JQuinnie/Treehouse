9f79e3bdc72bad0926351a9ff3e0d4b5217aaf5b
JQuinnie <jenn8866@gmail.com>
2018-01-04 15:09:10 -0600

    adding error handlers

--- qa-rest-api/app.js
+++ qa-rest-api/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes.js');
+
 const jsonParser = require('body-parser').json; // function that will return middleware
 const logger = require('morgan'); // middleware logger
 
@@ -11,11 +12,28 @@ app.use(jsonParser());
 
 app.use('/questions', routes); // setting up route
 
-// adding middleware to extend functionality of express app
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  req.body;
-  next();
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// Custom Error Handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500); // 500 is internal server error
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+// // adding middleware to extend functionality of express app
+// app.use(function(req, res, next) {
+//   req.body;
+//   next();
+// });
 
 // specify server port
 const port = process.env.PORT || 3000;
