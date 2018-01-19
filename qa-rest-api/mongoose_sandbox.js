ead685f988926edca9f57513b59cc6d57e4d094d
JQuinnie <jenn8866@gmail.com>
2018-01-19 14:06:45 -0600

    add static and instance methods using mongoose plus pre hooks

--- qa-rest-api/mongoose_sandbox.js
+++ qa-rest-api/mongoose_sandbox.js
@@ -21,10 +21,7 @@ db.once('open', function () {
       type: String,
       default: 'goldfish'
     },
-    size: {
-      type: String,
-      default: 'small'
-    },
+    size: String,
     color: {
       type: String,
       default: 'golden'
@@ -38,13 +35,41 @@ db.once('open', function () {
       default: 'Anglea'
     }
   });
+
+  // pre hook middleware on schema
+  AnimalSchema.pre('save', function (next) {
+    if (this.mass >= 100) {
+      this.size = 'big';
+    } else if (this.mass >= 5 && this.mass < 100) {
+      this.size = 'medium';
+    } else {
+      this.size = 'small';
+    }
+    next();
+  });
+
+  // static method
+  AnimalSchema.statics.findSize = function (size, callback) {
+    // this == Animal
+    return this.find({
+      size: size // this is the query object
+    }, callback);
+  }
+
+  // instance method
+  AnimalSchema.methods.findSameColor = function (callback) {
+    //this == document
+    return this.model('Animal').find({
+      color: this.color
+    }, callback);
+  }
+
   // mongoose object model
   var Animal = mongoose.model('Animal', AnimalSchema);
 
   var elephant = new Animal({
     type: 'elephant',
-    size: 'big',
-    color: 'grey',
+    color: 'gray',
     mass: 6000,
     name: 'Lawrence'
   });
@@ -53,30 +78,49 @@ db.once('open', function () {
 
   var whale = new Animal({
     type: 'whale',
-    size: 'big',
     mass: 190500,
     name: 'Fig'
   });
 
+  var animalData = [{
+      type: 'mouse',
+      color: 'gray',
+      mass: 0.035,
+      name: 'Marvin'
+    },
+    {
+      type: 'nutria',
+      color: 'brown',
+      mass: 6.35,
+      name: 'Gretchen'
+    },
+    {
+      type: 'wolf',
+      color: 'gray',
+      mass: 45,
+      name: 'Iris'
+    },
+    elephant,
+    animal,
+    whale
+  ]
+
   //dump the initial animal, elephant so there are no duplicates (structured due to async without using promises)
   Animal.remove({}, function (err) {
     if (err) console.error(err);
-    elephant.save(function (err) {
+    Animal.create(animalData, function (err, animals) {
       if (err) console.error(err);
-      animal.save(function (err) {
-        if (err) console.error(err);
-        whale.save(function (err) {
+      // read all big animals
+      Animal.findOne({
+        type: 'elephant'
+      }, function (err, elephant) {
+        elephant.findSameColor(function (err, animals) {
           if (err) console.error(err);
-          // read all big animals
-          Animal.find({
-            size: 'big'
-          }, function (err, animals) {
-            animals.forEach(function (animal) {
-              console.log(animal.name + ' the ' + animal.color + ' ' + animal.type);
-            });
-            db.close(function () { //closed on the callback due to async
-              console.log('db connection closed');
-            });
+          animals.forEach(function (animal) {
+            console.log(animal.name + ' the ' + animal.color + ' ' + animal.type + ' is a ' + animal.size + '-sized animal.');
+          });
+          db.close(function () { //closed on the callback due to async
+            console.log('db connection closed');
           });
         });
       });
