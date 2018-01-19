a98f0f92bfcf18436547b7ca81852cb8f5393eab
JQuinnie <jenn8866@gmail.com>
2018-01-19 14:35:54 -0600

    adding sort functionality

--- qa-rest-api/models.js
+++ qa-rest-api/models.js
@@ -4,6 +4,25 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+// sort function for answers
+var sortAnswers = function (a, b) {
+  // - negative a before b
+  // 0 no change
+  // + positive a after b
+
+  if (a.votes === b.votes) {
+    // if (a.updatedAt > b.updatedAt) {
+    //   return -1;
+    // } else if (a.updatedAt < b.updatedAt) {
+    //   return 1;
+    // } else {
+    //   return 0;
+    // }
+    return b.updatedAt - a.updatedAt;
+  }
+  return b.votes - a.votes;
+}
+
 var AnswerSchema = new Schema({
   text: String,
   createdAt: {
@@ -20,6 +39,23 @@ var AnswerSchema = new Schema({
   }
 });
 
+//instance method
+AnswerSchema.method('update', function (updates, callback) {
+  Object.assign(this, updates, {
+    updatedAt: new Date()
+  });
+  this.parent().save(callback);
+});
+
+AnswerSchema.method('vote', function (vote, callback) {
+  if (vote === 'up') {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  thise.parent().save(callback);
+});
+
 var QuestionSchema = new Schema({
   text: String,
   createdAt: {
@@ -27,6 +63,11 @@ var QuestionSchema = new Schema({
     default: Date.now
   },
   answers: [AnswerSchema]
+});
+
+QuestionSchema.pre('save', function (next) {
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 var Question = mongoose.model('Question', QuestionSchema);
