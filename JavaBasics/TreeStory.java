fac61d0b6fac472c2126ad04ce13a177c34d520b
JQuinnie <jenn8866@gmail.com>
2018-04-14 22:26:28 -0600

    Change system to console

--- JavaBasics/TreeStory.java
+++ JavaBasics/TreeStory.java
@@ -5,6 +5,6 @@ public class TreeStory {
     Console console = System.console();
     String name = console.readLine("Enter your name:  ");
     String adjective = console.readLine("Enter an adjective:  ");
-    System.out.printf("%s is very %s", name, adjective);
+    console.printf("%s is very %s", name, adjective);
   }
 }
