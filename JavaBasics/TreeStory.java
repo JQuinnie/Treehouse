22f50c62f4f61c639d8619b6beed6406176f58c2
JQuinnie <jenn8866@gmail.com>
2018-04-14 22:34:18 -0600

    Updated story with multiple strings

--- JavaBasics/TreeStory.java
+++ JavaBasics/TreeStory.java
@@ -3,8 +3,13 @@ import java.io.*;
 public class TreeStory {
   public static void main(String args[]) {
     Console console = System.console();
-    String name = console.readLine("Enter your name:  ");
+    String name = console.readLine("Enter a name:  ");
     String adjective = console.readLine("Enter an adjective:  ");
-    console.printf("%s is very %s", name, adjective);
+    String noun = console.readLine("Enter a noun:  ");
+    String adverb = console.readLine("Enter an adverb:  ");
+    String verb = console.readLine("Enter a verb ending in -ing:  ");
+    console.printf("Your TreeStory:\n------------\n");
+    console.printf("%s is a(n) %s %s. ", name, adjective, noun);
+    console.printf("They are always %s %s.\n", adverb, verb);
   }
 }
