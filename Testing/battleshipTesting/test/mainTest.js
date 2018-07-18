90b574691f5ac2329f1e4474a1604267ba7e5d5e
JQuinnie <jenn8866@gmail.com>
2018-07-18 11:30:35 -0600

    Trivial func or test that proves things are set up correctly

--- Testing/battleshipTesting/test/mainTest.js
+++ Testing/battleshipTesting/test/mainTest.js
@@ -1,11 +1,12 @@
+// trivial function or test that proves things are set up correctly
 // import Chai
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
 // Test suite (block of unit test closely related)
-describe('Mocha', function(){
+describe("Mocha", function() {
   // sanity check
   // test spec (unit test)
-  it('should run our tests using npm', function(){
+  it("should run our tests using npm", function() {
     // testing something that will pass
     // ok is an insertion method for Chai, tests value if truthy
     expect(true).to.be.ok;
