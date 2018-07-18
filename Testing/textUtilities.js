dd3a5051673f588cf65c756a207903165d3f0305
JQuinn MacBook Air <jenn8866@gmail.com>
2018-07-17 23:34:54 -0600

    Update to make a word pass test

--- Testing/textUtilities.js
+++ Testing/textUtilities.js
@@ -1,11 +1,12 @@
 const expect = require("chai").expect;
 
 function titleCase(title) {
-  return title.toUpperCase();
+  return title[0].toUpperCase() + title.substring(1);
 }
 
 expect(titleCase("the shawshank redemption")).to.be.a("string");
 expect(titleCase("a")).to.equal("A");
+expect(titleCase("titanic")).to.equal("Titanic");
 // expect(titleCase("the shawshank redemption")).to.equal(
 //   "The Shawshank Redemption"
 // );
