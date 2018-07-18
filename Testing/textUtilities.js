9f1c5bca043f3e4a9a053f7abedd7a7d405517e9
JQuinn MacBook Air <jenn8866@gmail.com>
2018-07-17 23:30:03 -0600

    Start small with a simple test

--- Testing/textUtilities.js
+++ Testing/textUtilities.js
@@ -1,7 +1,11 @@
 const expect = require("chai").expect;
 
 function titleCase(title) {
-  return title;
+  return title.toUpperCase();
 }
 
-expect(titleCase("shawshank redemption")).to.be.a("string");
+expect(titleCase("the shawshank redemption")).to.be.a("string");
+expect(titleCase("a")).to.equal("A");
+// expect(titleCase("the shawshank redemption")).to.equal(
+//   "The Shawshank Redemption"
+// );
