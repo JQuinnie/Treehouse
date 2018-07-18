4acb6f69e6cbfcfecc07a83baac467f2e9bbc03c
JQuinnie <jenn8866@gmail.com>
2018-07-18 11:39:17 -0600

    Set up files to perform sanity check, export module to link

---
+++ Testing/battleshipTesting/test/ship_test.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+
+// set up new suite for function
+describe("checkForShip", function() {
+  const checkForShip = require("../gameLogic/shipMethods").checkForShip;
+
+  it("should correctly report no ship at a given players coordinate", function() {});
+});
