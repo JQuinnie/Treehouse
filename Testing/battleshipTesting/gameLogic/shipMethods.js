f8d53b1105c3996cdbed9f1c9f7fc9202c8477da
JQuinnie <jenn8866@gmail.com>
2018-07-18 12:00:53 -0600

    Set up function to check player and ship coordinates, if exists

--- Testing/battleshipTesting/gameLogic/shipMethods.js
+++ Testing/battleshipTesting/gameLogic/shipMethods.js
@@ -1,3 +1,23 @@
-function checkForShip() {}
+function checkForShip(player, coordinates) {
+  let shipPresent, ship;
+
+  // only want to return false if no ships at currently coordinates
+  for (let i = 0; i < player.ships.length; i++) {
+    ship = player.ships[i];
+
+    // compare each value to the given coordinates
+    shipPresent = ship.locations.filter(function(actualCoordinate) {
+      // filter current ship location array for matches against given coordinates
+      return (
+        actualCoordinate[0] === coordinates[0] &&
+        actualCoordinate[1] === coordinates[1]
+      );
+    })[0]; // be a coordinate if only a match, first unmatch will be undefined with empty array
+
+    if (!shipPresent) {
+      return false;
+    }
+  }
+}
 
 module.exports.checkForShip = checkForShip;
--- Testing/battleshipTesting/test/ship_test.js
+++ Testing/battleshipTesting/test/ship_test.js
@@ -4,5 +4,14 @@ const expect = require("chai").expect;
 describe("checkForShip", function() {
   const checkForShip = require("../gameLogic/shipMethods").checkForShip;
 
-  it("should correctly report no ship at a given players coordinate", function() {});
+  it("should correctly report no ship at a given players coordinate", function() {
+    player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
 });
