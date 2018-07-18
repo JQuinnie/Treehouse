ab094b95c000d44a8ade4b90e286177aaf3556bf
JQuinnie <jenn8866@gmail.com>
2018-07-18 12:16:22 -0600

    Add additional ship and check test

--- Testing/battleshipTesting/test/ship_test.js
+++ Testing/battleshipTesting/test/ship_test.js
@@ -51,6 +51,9 @@ describe("checkForShip", function() {
         },
         {
           locations: [[1, 0], [1, 1]]
+        },
+        {
+          locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
         }
       ]
     };
@@ -58,6 +61,7 @@ describe("checkForShip", function() {
     expect(checkForShip(player, [0, 1])).to.be.true;
     expect(checkForShip(player, [1, 0])).to.be.true;
     expect(checkForShip(player, [1, 1])).to.be.true;
+    expect(checkForShip(player, [2, 3])).to.be.true;
     expect(checkForShip(player, [9, 9])).to.be.false;
   });
 });
