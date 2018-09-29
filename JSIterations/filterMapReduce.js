575dd0c411ec1dbfd53980ec08bf827fef4991aa
jquinnie <jenn8866@gmail.com>
2018-09-29 13:48:55 -0600

    Add array filter example

---
+++ JSIterations/filterMapReduce.js
@@ -0,0 +1,7 @@
+// filter
+// callback returns true or false
+const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+
+const sNames = names.filter(name => name.charAt(0) === 'S');
+
+console.log(sNames);
