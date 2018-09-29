4f583e037394f5cf86089567587d3f0a42e370f4
jquinnie <jenn8866@gmail.com>
2018-09-29 14:07:41 -0600

    Add example to filter even numbers

--- JSIterations/filterMapReduce.js
+++ JSIterations/filterMapReduce.js
@@ -6,3 +6,9 @@ const startsWithS = name => name.charAt(0) === 'S';
 const sNames = names.filter(startsWithS);
 
 console.log(sNames);
+
+// filter even numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNums = numbers.filter(num => num % 2 === 0);
+
+console.log(evenNums);
