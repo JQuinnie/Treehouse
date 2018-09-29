e6d761aedda6cc6e2e1699c2a3341b7c1978a579
jquinnie <jenn8866@gmail.com>
2018-09-29 13:05:47 -0600

    Add forEach example

--- JSIterations/forEach.js
+++ JSIterations/forEach.js
@@ -11,3 +11,12 @@ for (let i = 0; i < fruits.length; i += 1) {
 // avoids increment mistakes and wrong conditions
 // can't break out
 fruits.forEach(fruit => console.log(fruit));
+
+const capFruits = [];
+
+fruits.forEach((fruit) => {
+  const capFruit = fruit.toUpperCase();
+  capFruits.push(capFruit);
+});
+
+console.log(capFruits);
