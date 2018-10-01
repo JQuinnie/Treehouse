9ef00706643b07ede3bd9e880f76353efe85c26a
JQuinn MacBook Air <jenn8866@gmail.com>
2018-09-30 20:45:51 -0600

    Add example of filter with array of objects

--- JSIterations/combineArray.js
+++ JSIterations/combineArray.js
@@ -4,8 +4,15 @@
 const numbers = [1, 1, 2, 3, 4, 3, 5, 5, 6, 7, 3, 8, 9, 10];
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
 //the indexOf method will return the index of the first occurrence that is found in the array
-const unique = numbers.filter(function(number, index, array) {
+const unique = numbers.filter((number, index, array) => {
   return index === array.indexOf(number);
 });
 
 console.log(unique);
+
+// working with objects in array
+const users = [{ name: "Samir" }, { name: "Angela" }, { name: "Beatrice" }];
+
+const newUsers = users.filter(user => user.name !== "Samir");
+
+console.log(newUsers);
