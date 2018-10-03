eee34324ad9111cef099332c22df91cc8e59f4d6
jquinnie <jenn8866@gmail.com>
2018-10-03 15:04:47 -0600

    Refactored two promises resolved

--- JSOther/promiseExample.js
+++ JSOther/promiseExample.js
@@ -4,12 +4,21 @@ const calcPromise = new Promise((resolve, reject) => {
   }, 1000);
 });
 
-// one promise resolved
-calcPromise.then((value) => {
-  console.log(`The answer is ${value}`);
-});
+// // one promise resolved
+// calcPromise.then((value) => {
+//   console.log(`The answer is ${value}`);
+// });
 
-// two promise resolved
-calcPromise.then(value => value + 2).then((nextValue) => {
-  console.log(`The final value is ${nextValue}`);
-});
+// // two promise resolved
+// calcPromise.then(value => value + 2).then((nextValue) => {
+//   console.log(`The final value is ${nextValue}`);
+// });
+
+// refactored functions
+const addTwo = value => value + 2;
+
+const printFinalValue = (value) => {
+  console.log(`The final value is ${value}`);
+};
+
+calcPromise.then(addTwo).then(printFinalValue);
