fa5147ccde3756dcbbd7964e02cc85a03b947f60
jquinnie <jenn8866@gmail.com>
2018-10-03 15:00:53 -0600

    Add example of two promise resolved

--- JSOther/promiseExample.js
+++ JSOther/promiseExample.js
@@ -4,6 +4,12 @@ const calcPromise = new Promise((resolve, reject) => {
   }, 1000);
 });
 
+// one promise resolved
 calcPromise.then((value) => {
   console.log(`The answer is ${value}`);
+});
+
+// two promise resolved
+calcPromise.then(value => value + 2).then((nextValue) => {
+  console.log(`The final value is ${nextValue}`);
 });
