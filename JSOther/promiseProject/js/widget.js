dd42eb6e981898f2508a839869fbbd0400de5fde
jquinnie <jenn8866@gmail.com>
2018-10-03 15:45:55 -0600

    Add updates to code and flow

--- JSOther/promiseProject/js/widget.js
+++ JSOther/promiseProject/js/widget.js
@@ -1,15 +1,25 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '../data/employees.json');
-xhr.onreadystatechange = handleResponse;
-xhr.send();
+const getJSON = url => new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onreadystatechange = handleResponse;
+  xhr.onerror = (error) => {
+    reject(error);
+  };
+  xhr.send();
 
-function handleResponse() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const employees = JSON.parse(xhr.responseText);
-    addEmployeesToPage(employees);
+  function handleResponse() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const employees = JSON.parse(xhr.responseText);
+        resolve(employees);
+      } else {
+        reject(this.statusText);
+      }
+    }
   }
-}
+});
 
+// takes in employees and returns list items
 function generatListItems(employees) {
   let statusHTML = '';
   for (let i = 0; i < employees.length; i += 1) {
@@ -25,12 +35,23 @@ function generatListItems(employees) {
   return statusHTML;
 }
 
+// takes in list items and returns final HTML to append to DOM
 function generateUnorderedList(listItems) {
   return `<ul class="bulleted">${listItems}</ul>`;
 }
 
-function addEmployeesToPage(employees) {
-  document.getElementById('employeeList').innerHTML = generateUnorderedList(
-    generatListItems(employees),
-  );
+// Appending unordered list to DOM
+function addEmployeesToPage(unorderedList) {
+  document.getElementById('employeeList').innerHTML = unorderedList;
 }
+
+const ajaxPromise = getJSON('../data/employees.json');
+
+// new promise return items will be HTML snippets with all list items, then wrap unordered list around it, then append the list to the page, last is to catch all errors thrown to code
+ajaxPromise
+  .then(generatListItems)
+  .then(generateUnorderedList)
+  .then(addEmployeesToPage)
+  .catch((error) => {
+    console.log(error);
+  });
