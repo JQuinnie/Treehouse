96dcfd8d9f9c56f7a66d202ec566822a110d7b8f
jquinnie <jenn8866@gmail.com>
2018-10-03 17:08:48 -0600

    Update fix of cross domain request

--- JSOther/promiseProject/js/widget.js
+++ JSOther/promiseProject/js/widget.js
@@ -24,14 +24,6 @@ function addEmployeesToPage(unorderedList) {
   document.getElementById('employeeList').innerHTML = unorderedList;
 }
 const getJSON = url => new Promise((resolve, reject) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onreadystatechange = handleResponse;
-  xhr.onerror = (error) => {
-    reject(error);
-  };
-  xhr.send();
-
   function handleResponse() {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
@@ -42,12 +34,23 @@ const getJSON = url => new Promise((resolve, reject) => {
       }
     }
   }
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onreadystatechange = handleResponse;
+  xhr.onerror = (error) => {
+    reject(error);
+  };
+  xhr.send();
 });
 
-const ajaxPromise = getJSON('../data/employees.json');
+const ajaxPromise = getJSON(
+  'https://cors.io/?http://port-80-zv8pzn9eex.treehouse-app.com/data/employees.json',
+);
 
 // new promise return items will be HTML snippets with all list items, then wrap unordered list around it, then append the list to the page, last is to catch all errors thrown to code
 ajaxPromise
+  .then(console.log('getting into promiseland'))
   .then(generatListItems)
   .then(generateUnorderedList)
   .then(addEmployeesToPage)
