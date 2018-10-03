54d124c9302efec0ee7f5979b2922710caa75e01
jquinnie <jenn8866@gmail.com>
2018-10-03 15:12:55 -0600

    Add real world project example for promise

---
+++ JSOther/promiseProject/js/widget.js
@@ -0,0 +1,36 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '../data/employees.json');
+xhr.onreadystatechange = handleResponse;
+xhr.send();
+
+function handleResponse() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const employees = JSON.parse(xhr.responseText);
+    addEmployeesToPage(employees);
+  }
+}
+
+function generatListItems(employees) {
+  let statusHTML = '';
+  for (let i = 0; i < employees.length; i += 1) {
+    if (employees[i].inoffice === true) {
+      statusHTML += '<li class="in">';
+    } else {
+      statusHTML += '<li class="out">';
+    }
+    statusHTML += employees[i].name;
+    statusHTML += '</li>';
+  }
+
+  return statusHTML;
+}
+
+function generateUnorderedList(listItems) {
+  return `<ul class="bulleted">${listItems}</ul>`;
+}
+
+function addEmployeesToPage(employees) {
+  document.getElementById('employeeList').innerHTML = generateUnorderedList(
+    generatListItems(employees),
+  );
+}
